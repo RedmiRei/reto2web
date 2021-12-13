0794bdf45bfce1052266939e961cdd1609706d9d
yeisi.contreras.mt@correo.usa.edu.co <yeisi.contreras.mt@correo.usa.edu.co>
2021-12-13 17:10:32 -0500

    Mod Reto1

--- src/main/java/com/ciclocuatro/retos/reto/service/UserService.java
+++ src/main/java/com/ciclocuatro/retos/reto/service/UserService.java
@@ -46,15 +46,19 @@ public class UserService {
     }
 
     public User create(User user){
-        if (user.getId() != null){
-            Optional<User> revision= repositorio.getUser(user.getId());
-            if (revision.isEmpty()){
-                return repositorio.create(user);
+        if (user.getId() == null) {
+            return user;
+        }else {
+            Optional<User> revision = repositorio.getUser(user.getId());
+            if (!revision.isPresent()) {
+                if (emailExists(user.getEmail())==false){
+                    return repositorio.create(user);
+                }else{
+                    return user;
+                }
             }else{
                 return user;
             }
-        }else {
-            return user;
         }
     }
 
@@ -84,7 +88,9 @@ public class UserService {
                 if (user.getZone() != null) {
                     userDb.get().setZone(user.getZone());
                 }
-
+                if (user.getType() != null) {
+                    userDb.get().setType(user.getType());
+                }
                 repositorio.update(userDb.get());
                 return userDb.get();
             } else {
